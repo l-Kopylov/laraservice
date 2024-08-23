2c042aeb0aedabbfb697eabe8e428961246753c8
l-Kopylov <leonidinterprice@gmail.com>
2024-08-23 20:08:33 +0200

    миграция и модель материала

---
+++ database/migrations/2024_08_23_180240_create_materials_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('price_per_square_meter'); // Цена за квадратный метр 
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+};
